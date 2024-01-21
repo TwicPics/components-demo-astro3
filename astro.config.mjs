46787356273d9af537ad1eba394de0fa32ca8b3e
Miguel Beignon <mbgspcii@gmail.com>
2024-01-21 11:58:41 +0100

    Upgrades dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import react from '@astrojs/react'
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), react()]
+	integrations: [icon(), react(), tailwind(),]
 })
